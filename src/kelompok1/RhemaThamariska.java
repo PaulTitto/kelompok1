0ef463900a7790da3a4eb88b2fa0cb29b1be00d8
rhemathamariska <111751423+rhemathamariska@users.noreply.github.com>
2023-03-30 09:33:26 +0700

    .
    Punya Rhema

---
+++ src/kelompok1/RhemaThamariska.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelompok1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class RhemaThamariska { 
+    //methods//
+    void rhema(){
+        System.out.println("HALO AKU REMA");
+    }
+}
