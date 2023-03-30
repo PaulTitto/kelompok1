c394ae0c3543ff7a1467df26f4ac588469924fa1
rhemathamariska <111751423+rhemathamariska@users.noreply.github.com>
2023-03-30 09:57:42 +0700

    .
    punyarhema2

--- src/kelompok1/RhemaThamariska.java
+++ src/kelompok1/RhemaThamariska.java
@@ -9,7 +9,15 @@ package kelompok1;
  * @author ASUS
  */
 public class RhemaThamariska { 
+    int pjg1;
+    int pjg2;
+    void sum(){
+        int result = this.pjg1 + this.pjg2;
+        System.out.println("Hasilnya?" + result );
+    }
     public static void main(String[] args) {
-        System.out.println("HALO AKU REMA");
+        System.out.println("HALO AKU RHEMA");
+        System.out.println("Nama kamu siapa?");
+    
     }
 }
